a0bf0442f30c808cb2e985cc5ea885cf39b81a62
nithishwaran-labhub <nithishwaran321io@gmail.com>
2025-11-19 20:59:58 +0530

    Update wishlist.js

--- wishlist.js
+++ wishlist.js
@@ -1,1 +1,20 @@
+let wishlist = JSON.parse(localStorage.getItem("wishlist")) || [];
+
+let container = document.getElementById("wishlist-items");
+
+function displayWishlist() {
+  container.innerHTML = "";
+
+  wishlist.forEach(item => {
+    container.innerHTML += `
+      <div class="product">
+        <img src="${item.img}">
+        <h3>${item.name}</h3>
+        <p>₹${item.price}</p>
+      </div>
+    `;
+  });
+}
+
+displayWishlist();
 
