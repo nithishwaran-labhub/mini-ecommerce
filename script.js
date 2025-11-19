157354fc72ca889b12a81e09697be978fd6050fb
nithishwaran-labhub <nithishwaran321io@gmail.com>
2025-11-19 19:54:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+let products = [
+  {id: 1, name: "Shoes", price: 999, img: "products/item1.jpg"},
+  {id: 2, name: "Watch", price: 699, img: "products/item2.jpg"},
+  {id: 3, name: "Bag", price: 499, img: "products/item3.jpg"},
+];
+
+let productList = document.getElementById("product-list");
+
+products.forEach((p) => {
+  productList.innerHTML += `
+    <div class="product">
+      <img src="${p.img}">
+      <h3>${p.name}</h3>
+      <p>₹${p.price}</p>
+      <button onclick="addToCart(${p.id})">Add to Cart</button>
+    </div>
+  `;
+});
+
+function addToCart(id) {
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+  let product = products.find((p) => p.id === id);
+
+  cart.push(product);
+  localStorage.setItem("cart", JSON.stringify(cart));
+
+  alert("Added to cart!");
+}
 
